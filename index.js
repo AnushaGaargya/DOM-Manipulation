64935aa12dd928d1d217f025a8ee2a20e25801bf
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-08 23:09:23 +0530

    commit2

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-lstelement  = document.getElementsByClassName("list-group-item")
-lstelement[2].style.backgroundColor = "green"
+const lstelement  = document.getElementsByClassName("list-group-item");
+lstelement[2].style.backgroundColor = "green";
 
 for (let i=0; i<=lstelement.length; i++){
-  var string = lstelement[i].innerHTML
-  lstelement[i].innerHTML = string.bold()
+ 
+    s = (lstelement[i]).innerHTML;
+    (lstelement[i]).innerHTML = s.bold();
+   
   
 }
+
+
+// [...lstelement].forEach((element, index, array) => {
+//     // do something
+//     s = (lstelement[index]).innerHTML
+//     (lstelement[index]).innerHTML = s.bold()
+// });
+
+list = document.getElementsByTagName("li")
+console.log(list)
 
