b8e4b55105b2a1a81aaeb0d48515264bb9d80a82
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-09 13:44:32 +0530

    parent,sibling,appendchild,createelement

--- index.js
+++ index.js
@@ -16,14 +16,46 @@ const lstelement  = document.getElementsByClassName("list-group-item");
 //     (lstelement[index]).innerHTML = s.bold()
 // });
 
-list = document.getElementsByTagName("li")
-console.log(list)
+// list = document.getElementsByTagName("li")
+// console.log(list)
 
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
 // var listitems = document.querySelectorAll(".list-group-item");
 // console.log(listitems)
-for(var i=0; i<=odd.length; i++){
-   odd[i].style.color = 'green';
-    }
+// for(var i=0; i<=odd.length; i++){
+//    odd[i].style.color = 'green';
+//     }
+
+// parentnode property
+var itemList = document.querySelector('#items');
+console.log(itemList.parentNode)
+itemList.parentNode.style.backgroundColor = "#f4f4f4";
+
+// (itemList.children[0]).style.backgroundColor = "green"; 
+
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = "Hello World 1";
+itemList.firstElementChild.style.backgroundColor = "yellow";
+console.log(itemList.lastElementChild);
+itemList.lastElementChild .textContent = "Hello World 4"; 
+// console.log(itemList.lastElementChild);
+var x  = document.querySelector("#items");
+console.log(x.parentNode);
+console.log(x.firstElementChild.nextElementSibling.previousElementSibling);
+console.log(x.previousElementSibling);
+
+var newhead = document.createElement('h1');
+newhead.className = 'hellohead';
+newhead.setAttribute('title', 'HelloWorld');
+var newheadText = document.createTextNode('Hello World');
+newhead.appendChild(newheadText);    
+console.log(newhead);  
+
+var container  = document.querySelector('body .card-body')
+var h1 = document.querySelector('body h2');
+container.insertBefore(newhead, h1)
+  
+  
 
