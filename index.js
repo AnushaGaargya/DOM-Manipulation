40d146b7dc4082957aba511cabd368b439670606
Anusha Gaargya <anushasgarg@gmail.com>
2023-06-09 12:21:25 +0530

    queryselectorAll-odd

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const lstelement  = document.getElementsByClassName("list-group-item");
-lstelement[2].style.backgroundColor = "green";
+// lstelement[2].style.backgroundColor = "green";
 
-for (let i=0; i<=lstelement.length; i++){
+// for (let i=0; i<=lstelement.length; i++){
  
-    s = (lstelement[i]).innerHTML;
-    (lstelement[i]).innerHTML = s.bold();
+//     s = (lstelement[i]).innerHTML;
+//     (lstelement[i]).innerHTML = s.bold();
    
   
-}
+// }
 
 
 // [...lstelement].forEach((element, index, array) => {
@@ -18,4 +18,12 @@ for (let i=0; i<=lstelement.length; i++){
 
 list = document.getElementsByTagName("li")
 console.log(list)
+
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+// var listitems = document.querySelectorAll(".list-group-item");
+// console.log(listitems)
+for(var i=0; i<=odd.length; i++){
+   odd[i].style.color = 'green';
+    }
 
